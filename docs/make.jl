bc0a87e65518a79f7ec53ae98cf9fdac1caf9767
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-04-13 13:57:04 +1000

    Update to Documenter v0.24

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,12 @@
 using Documenter, IntervalTrees
 
 makedocs(
+    format = Documenter.HTML(
+        edit_link = "master",
+        sidebar_sitename = false
+    ),
     modules = [IntervalTrees],
-    format = :html,
-    sitename = "IntervalTrees",
+    sitename = "IntervalTrees.jl",
     pages = [
         "Home" => "index.md",
         "Manual" => [
@@ -14,14 +17,11 @@ makedocs(
             "Intersection" => "man/intersection.md"
         ]
     ],
-    authors = "Daniel Jones, Kenta Sato, Ben J. Ward, The BioJulia Organisation and other contributors."
+    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors."
 )
 
 deploydocs(
     repo = "github.com/BioJulia/IntervalTrees.jl.git",
-    julia = "0.7",
-    osname = "linux",
-    target = "build",
-    deps = nothing,
-    make = nothing
+    devbranch = "master",
+    push_preview = true
 )
